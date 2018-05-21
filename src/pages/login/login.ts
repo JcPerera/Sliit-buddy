db5dddfddcb01389ee474143e4a989fb002f45ba
Janaka Perera <it15110008@my.sliit.lk>
2018-05-21 14:38:11 +0530

    added instructor login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { GooglePlus } from "@ionic-native/google-plus";
 import * as firebase from 'firebase';
+import { LoadingController, AlertController } from 'ionic-angular';
 /**
  * Generated class for the LoginPage page.
  *
@@ -18,25 +19,27 @@ export class LoginPage {
 
   public fireAuth: any;
   public userProfile: any;
+  public database: any;
+  private loading: any;
   user;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public gplus: GooglePlus,
-    public platform: Platform
+    public platform: Platform,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController
   ) {
     this.fireAuth = firebase.auth();
     this.userProfile = firebase.database().ref('users');
+    this.database = firebase.database();
     this.user = this.fireAuth.currentUser;
     console.log(this.user);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-
   login = () => {
+    this.displayPreloader('Signing In User');
     if (this.platform.is('cordova')) {
       this.nativeGoogleLogin();
     } else {
@@ -61,7 +64,9 @@ export class LoginPage {
       this.userProfile.child(credential.uid).update({
         email: credential.email,
         username: credential.displayName,
-        photo: credential.photoURL
+        photo: credential.photoURL,
+        loggedAs: "student"
+
       }).then(() => {
 
       }, error => {
@@ -74,7 +79,6 @@ export class LoginPage {
     }
   }
 
-
   async webGoogleLogin(): Promise<void> {
     try {
       const provider = new firebase.auth.GoogleAuthProvider();
@@ -85,6 +89,8 @@ export class LoginPage {
         email: credential.user.email,
         username: credential.user.displayName,
         photo: credential.user.photoURL,
+        loggedAs: "student"
+
       }).then(() => {
 
       }, error => {
@@ -93,6 +99,120 @@ export class LoginPage {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  instructorLogin = () => {
+    this.displayPreloader('Signing In Instructor');
+    if (this.platform.is('cordova')) {
+      this.nativeIns();
+    } else {
+      this.webIns();
+    }
+  }
+
+  async nativeIns() {
+    try {
+      const gplusUser = await this.gplus.login({
+        'webClientId': '872519563666-3lcrq815fdf308oo489jm3g21nmjp3vt.apps.googleusercontent.com',
+        'offline': true,
+        'scopes': 'profile email'
+      })
+
+      await this.fireAuth.signInWithCredential(
+        firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken)
+      ).then((k) => {
+        this.database.ref('users/' + k.uid).once('value').then((snapshot) => {
+          try {
+            if (snapshot.val().type == "instructor") {
+              console.log(snapshot.key);
+              this.userProfile.child(snapshot.key).update({
+                email: k.email,
+                username: k.displayName,
+                photo: k.photoURL,
+                loggedAs: "instructor"
+              }).then(() => {
+                console.log('You are now logged in as an instructor');
+              }), error => {
+                console.log(error);
+              }
+            } else {
+              this.fireAuth.signOut();
+              this.gplus.logout();
+              this.displayAlert('Warning', 'You are not an instructor please login as a student')
+            }
+          } catch{
+            this.fireAuth.signOut();
+            this.gplus.logout();
+            this.displayAlert('Warning', 'You are not an instructor please login as a student')
+          }
+        })
+
+
+        console.log(k)
+      })
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async webIns() {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      await this.fireAuth.signInWithPopup(provider).then((k) => {
+        this.database.ref('users/' + k.user.uid).once('value').then((snapshot) => {
+          try {
+            if (snapshot.val().type == "instructor") {
+              console.log(snapshot.key);
+              this.userProfile.child(snapshot.key).update({
+                email: k.user.email,
+                username: k.user.displayName,
+                photo: k.user.photoURL,
+                loggedAs: "instructor"
+              }).then(() => {
+                console.log('You are now logged in as an instructor');
+              }), error => {
+                console.log(error);
+              }
+            } else {
+              this.fireAuth.signOut();
+              this.displayAlert('Warning', 'You are not an instructor please login as a student')
+            }
+          } catch{
+            this.fireAuth.signOut();
+            this.displayAlert('Warning', 'You are not an instructor please login as a student')
+          }
+
+        })
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  displayPreloader(msg: any): void {
+    this.loading = this.loadingCtrl.create({
+      dismissOnPageChange: true,
+      content: msg
+    });
+
+    this.loading.present();
+  }
+
+
+
+  hidePreloader(): void {
+    this.loading.dismiss();
+  }
+
+  displayAlert(title: any, msg: any) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
